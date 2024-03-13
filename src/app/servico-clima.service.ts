b1eabb15cbc5163548da9d2f724d3e17dfd960a3
Daniel <danielgondimdecastro@gmail.com>
2024-03-13 20:54:49 -0300

    feat: importa httpClient e adiciona método que busca informações climáticas da cidade

--- src/app/servico-clima.service.ts
+++ src/app/servico-clima.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ServicoClimaService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPrevisaoClima(lat: number, lon: number, part: string, apiKey: string) {
+
+    const urlApi = 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}';
+
+    return this.http.get(urlApi);
+  }
 }
