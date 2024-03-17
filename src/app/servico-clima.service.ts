a870a2bd98983329efae8f88b57cb04858183d57
Daniel Gondim <danielgondimdecastro@gmail.com>
2024-03-17 14:16:27 -0300

    feat: cria a interface PrevisaoClima com os atributos temperatura, condicao, sensacaoTermica e umidade
    -corrige a URL de conexão com a API para buscar as informações climáticas
    
    -tipa corretamente a resposta da API com a interface PrevisaoClima:

--- src/app/servico-clima.service.ts
+++ src/app/servico-clima.service.ts
@@ -1,5 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {Observable} from "rxjs";
+
+export interface PrevisaoClima {
+  temperatura: number;
+  condicao: string;
+  sensaçãoTermica: number;
+  umidade :number
+}
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +17,14 @@ export class ServicoClimaService {
 
   constructor(private http: HttpClient) { }
 
-  getPrevisaoClima(lat: number, lon: number, part: string, apiKey: string) {
+  getPrevisaoClima(city: string, apiKey: string): Observable<PrevisaoClima> {
 
-    const urlApi = 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}';
+    const urlApi = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}&lang=pt_br';
 
-    return this.http.get(urlApi);
+    return this.http.get<PrevisaoClima>(urlApi);
   }
 }
+
+// 1dd0a6555a320942db378261b6ac18f2
+// https://api.openweathermap.org/data/2.5/weather?q=${goiania}&units=metric&appid=${1dd0a6555a320942db378261b6ac18f2}&lang=pt_br
+// https://api.openweathermap.org/data/2.5/weather?q=goiania&units=metric&appid=1dd0a6555a320942db378261b6ac18f2&lang=pt_br
