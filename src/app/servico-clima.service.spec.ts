19fb87a698f2f2b5188f3230a30282e2fe6aa621
Daniel Gondim <danielgondimdecastro@gmail.com>
2024-03-17 21:58:58 -0300

    test: Adiciona HttpClientTestingModule para resolver NullInjectorError

--- src/app/servico-clima.service.spec.ts
+++ src/app/servico-clima.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ServicoClimaService } from './servico-clima.service';
+import {HttpClient} from "@angular/common/http";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('ServicoClimaService', () => {
   let service: ServicoClimaService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ServicoClimaService]
+    });
     service = TestBed.inject(ServicoClimaService);
   });
 
