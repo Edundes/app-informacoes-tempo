89b8f68b211111af159c4ef9f8e55bb4335027f1
Daniel Gondim <danielgondimdecastro@gmail.com>
2024-03-18 13:56:08 -0300

    test: refatora o teste do componente InformacaoClimaComponent para usar fakeAsyng e tick - modifica configuração do módulo de teste para usar valor mockado do ServicoClimaService

--- src/app/informacao-clima/informacao-clima.component.spec.ts
+++ src/app/informacao-clima/informacao-clima.component.spec.ts
@@ -1,18 +1,23 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 import { InformacaoClimaComponent } from './informacao-clima.component';
 import {ServicoClimaService} from "../servico-clima.service";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {of} from "rxjs";
 
 describe('InformacaoClimaComponent', () => {
   let component: InformacaoClimaComponent;
   let fixture: ComponentFixture<InformacaoClimaComponent>;
+  let servicoClimaService: ServicoClimaService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      providers: [ServicoClimaService]
-    })
-    .compileComponents();
+      // declarations: [ InformacaoClimaComponent ],
+      providers: [
+        { provide: ServicoClimaService, useValue: {
+            getPrevisaoClima: jasmine.createSpy('getPrevisaoClima').and.returnValue(of({ cidade: 'Goiânia' }))
+          }}
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(InformacaoClimaComponent);
     component = fixture.componentInstance;
@@ -23,10 +28,11 @@ describe('InformacaoClimaComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should display the city name', () => {
+  it('should display the city name', fakeAsync(() => {
     component.cidadeInformada = 'Goiânia';
     fixture.detectChanges();
+    tick();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('h2').textContent).toContain('Goiânia');
-  });
+  }));
 });
