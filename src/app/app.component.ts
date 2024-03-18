bd99faebb42d3df3587b3de9e66a0d180c7077d1
Eduardo Mendes <eduardomendes392@gmail.com>
2024-03-18 20:48:24 -0300

    alterações nos arquivos ts do app e do seletor e no html do app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SeletorCidadeComponent } from "./seletor-cidade/seletor-cidade.component";
+import { InformacaoClimaComponent } from "./informacao-clima/informacao-clima.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, SeletorCidadeComponent]
+    imports: [RouterOutlet, SeletorCidadeComponent, InformacaoClimaComponent]
 })
 export class AppComponent {
   title = 'painel-meterorologico';
--- src/app/seletor-cidade/seletor-cidade.component.ts
+++ src/app/seletor-cidade/seletor-cidade.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ServicoClimaService } from '../servico-clima.service';
 
+
 @Component({
   selector: 'app-seletor-cidade',
   standalone: true,
@@ -17,7 +18,17 @@ constructor(private servicoClimaService: ServicoClimaService) {
 
 }
 procurarCidade() {
-
+ // Verifica se a cidade não está vazia antes de chamar o serviço
+ if (this.cidade.trim() !== '') {
+  // Chama o método do serviço para buscar as informações climáticas da cidade
+  this.servicoClimaService.getPrevisaoClima(this.cidade, '1dd0a6555a320942db378261b6ac18f2')
+  .subscribe(previsao => {
+      // Faça o que for necessário com os dados da previsão climática aqui
+      console.log(previsao);
+    });
+} else {
+  console.log('Por favor, insira o nome da cidade.');
+}
 }
 
 }
